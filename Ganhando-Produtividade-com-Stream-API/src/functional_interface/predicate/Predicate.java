d4aebc4a175d61de30f421f9acdfffbd3ad485d1
IcaroRaphael <icaro2551@gmail.com>
2023-09-13 17:51:51 -0300

    feat: Functional Interface - Predicate<T>

---
+++ Ganhando-Produtividade-com-Stream-API/src/functional_interface/predicate/Predicate.java
@@ -0,0 +1,24 @@
+package functional_interface.predicate;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Representa uma função que aceita um argumento do tipo T e retorna um valor booleano (verdadeiro oufalso).
+ * É comumente usada para filtrar os elementos do Stream com base em alguma condição.
+ */
+public class Predicate {
+  public static void main(String[] args) {
+    // Criar uma lista de palavras
+    List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
+
+    // Criar um Predicate que verifica se a palavra tem mais de 5 caracteres
+    java.util.function.Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+
+    // Usar o Stream para filtrar as palavras com mais de 5 caracteres e, em seguida,
+    // imprimir cada palavra que passou no filtro
+    palavras.stream()
+        .filter(maisDeCincoCaracteres)
+        .forEach(System.out::println);
+  }
+}
