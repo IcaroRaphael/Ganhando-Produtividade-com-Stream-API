57f7fc0452f83c9a5ec4d990123f9e837eaea7d8
IcaroRaphael <icaro2551@gmail.com>
2023-09-14 15:38:23 -0300

    Desafios - 04

---
+++ Ganhando-Produtividade-com-Stream-API/src/desafios/desafio_04/Main.java
@@ -0,0 +1,14 @@
+package desafios.desafio_04;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> numerosPares = numeros.stream().filter(n -> n % 2 == 0).toList();
+        numerosPares.forEach(System.out::println);
+    }
+}
