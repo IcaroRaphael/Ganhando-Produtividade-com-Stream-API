83112100791ac39b0fcbce50a1aa3bdf14b68c49
IcaroRaphael <icaro2551@gmail.com>
2023-09-14 15:11:47 -0300

    Desafios - 02

---
+++ Ganhando-Produtividade-com-Stream-API/src/desafios/desafio_02/Main.java
@@ -0,0 +1,15 @@
+package desafios.desafio_02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Integer soma = numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .reduce(0, Integer::sum);
+        System.out.println(soma);
+    }
+}
